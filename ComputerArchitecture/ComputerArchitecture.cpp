4860c0ebf0b7f5f65df1c802f1a2c9274939c463
delokoseni <107359196+delokoseni@users.noreply.github.com>
2023-03-25 19:57:54 +0700

    Паралельности нет

--- ComputerArchitecture/ComputerArchitecture.cpp
+++ ComputerArchitecture/ComputerArchitecture.cpp
@@ -2,29 +2,37 @@
 #include <Windows.h>
 #include <cmath>
 #include <ctime>
+#include <omp.h>
 using namespace std;
 
-int main(){
+int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int arr[1000], i, j, averagevalue, difference;
+	int* arr, i, j, averagevalue, difference, N = 100000;
+	arr = new int[N];
 	srand(time(NULL));
-	for (i = 0; i < 1000; i++)
-		arr[i] = rand() % 100;
+#pragma omp parallel
+	for (i = 0; i < N; i++)
+		arr[i] = rand() % N;
+	unsigned int start_time = clock();
 	averagevalue = arr[0];
-	for (i = 1; i < 1000; i++)
+#pragma omp parallel
+	for (i = 1; i < N; i++)
 		averagevalue += arr[i];
-	averagevalue /= 1000;
+	averagevalue /= N;
 	difference = abs(arr[0] - averagevalue);
 	j = 0;
-	for (i = 1; i < 1000; i++) {
+#pragma omp parallel
+	for (i = 1; i < N; i++) {
 		if (abs(arr[i] - averagevalue) < difference) {
 			j = i;
 			difference = abs(arr[i] - averagevalue);
 		}
 	}
+	unsigned int end_time = clock();
 	cout << "»ндекс элемента, наиболее близкого к среднему значению всех элементов массива: " << j << endl;
-	cout << "—реднее значение всех элементов массива: "<< averagevalue << endl;
+	cout << "—реднее значение всех элементов массива: " << averagevalue << endl;
 	cout << "Ёлемент, наиболее близкий к нему: " << arr[j] << endl;
+	cout << "¬рем€ выполнени€ основного алгоритма программы: " << end_time - start_time << endl;
 	return 0;
 }
